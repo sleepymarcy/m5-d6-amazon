e57be59a44e1c53dba3f942faadc5728127d3a45
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-10-25 12:40:52 +0200

    commit

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 // import productsRouter from './products/index.js'
 // import reviewsRoute from "./reviews/index.js"
-import { badRequestHandler, notFoundHandler,forbiddenErrorHandler, internalServerErrorHandler } from './reviews/errorHandlers.js'
+/* import { badRequestHandler, notFoundHandler,forbiddenErrorHandler, internalServerErrorHandler } from './reviews/errorHandlers.js' */
 
-const fname = fileURLToPath(import.meta.url)
-const dname = dirname(fname)
-const publicDirectory = path.join(dname, "../public")
+const fname = fileURLToPath(import.meta.url);
+const dname = dirname(fname);
+const publicDirectory = path.join(dname, "../public");
 
 const server = express();
 const port = 3010;
@@ -20,15 +20,16 @@ server.use(express.static(publicDirectory));
 // server.use('/products', productsRouter)
 // server.use("/reviews",reviewsRoute)
 
- server.use(badRequestHandler)
+/*  server.use(badRequestHandler)
  server.use(notFoundHandler)
  server.use(forbiddenErrorHandler)
  server.use(internalServerErrorHandler)
- console.table(listEndpoints(server))
+ console.table(listEndpoints(server)) */
 
- console.log(listEndpoints(server))
+console.log(listEndpoints(server));
 
 server.listen(port, () => console.log("Port running: ", port));
 
-server.on('error', (error) =>
-    console.log(`Server is not running due to : ${error}`))
+server.on("error", (error) =>
+  console.log(`Server is not running due to : ${error}`)
+);
